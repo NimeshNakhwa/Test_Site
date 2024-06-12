47b9b9be9ada2d03a8433413576af53fa705aab5
NimeshNakhwa <122160315+NimeshNakhwa@users.noreply.github.com>
2024-06-12 11:15:58 +0530

    Update and rename Drinker.html to script.js

---
+++ script.js
@@ -0,0 +1,92 @@
+n=73
+a=Number(prompt("Choose the Level :\n1] Easy : 15 Attempts.\n2] Medium : 10 Attempts.\n3] Hard : 5 Attempts"))
+if(a=1)
+{
+    console.log("Easy Level.\nYou Have 15 attempts.")
+    for(i=0;i<=15;i++)
+{
+    m=Number(prompt("Guess the Number(0 to 100) : "))
+    if((m>=0) & (m<=100))
+    {
+        if(m>n)
+    {
+        console.log(`Jackpot is Smaller than ${m}`)
+    }
+    else if(m<n)
+    {
+        console.log(`Jackpot is Greater than ${m}`)
+    }
+    else if(m=n)
+    {
+        console.log("YOU WON THE JACKPOT!!!")
+        break;
+    }
+    }
+    else
+    {
+        console.log("Invalid Input")
+    }
+}
+}
+else if(a=2)
+{
+    console.log("Medium Level.\nYou Have 10 attempts.")
+    for(i=0;i<=10;i++)
+{
+    m=Number(prompt("Guess the Number(0 to 100) : "))
+    if((m<=100)&(m>=0))
+    {
+        if(m>n)
+    {
+        console.log(`Jackpot is Smaller than ${m}`)
+    }
+    else if(m<n)
+    {
+        console.log(`Jackpot is Greater than ${m}`)
+    }
+    else if(m=n)
+    {
+        console.log("YOU WON THE JACKPOT!!!")
+        break;
+    }
+    }
+    else
+    {
+        console.log("Invalid Input")
+    }
+}
+}
+else if(a=3)
+{
+    console.log("Hard Level.\nYou Have 5 attempts.")
+    for(i=0;i<=5;i++)
+        {
+            m=Number(prompt("Guess the Number(0 to 100) : "))
+            if((m<=100) & m>=0)
+                {
+                    if(m>n)
+                        {
+                        console.log(`Jackpot is Smaller than ${m}`)
+                        }
+            else if(m<n)
+                        {
+                        console.log(`Jackpot is Greater than ${m}`)
+                        }
+            else if(m=n)
+                        {
+                        console.log("YOU WON THE JACKPOT!!!")
+                        break;
+                        }
+                }
+    else
+    {
+        console.log("Invalid Input.")
+    }
+}
+}
+else
+{
+    console.log("Invalid Input.")
+}
+
+
