22f427572eaf0e33e6859b30feb69ec36bc68c3f
NimeshNakhwa <122160315+NimeshNakhwa@users.noreply.github.com>
2024-06-12 11:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,106 +1,40 @@
-n=73
-a=Number(prompt("Choose the Level :\n1] Easy : 15 Attempts.\n2] Medium : 10 Attempts.\n3] Hard : 5 Attempts"))
-function startGame(level){
-switch(level) {
-    case 1:
-        b = 15;
-        break;
-    case 2:
-        b = 10;
-        break;
-    case 3:
-        b = 5;
-        break;
-    default:
-        b = 0;
-}
-if(a=1)
-{
-    console.log("Easy Level.\nYou Have 15 attempts.")
-    for(i=0;i<=15;i++)
-{
-    m=Number(prompt("Guess the Number(0 to 100) : "))
-    if((m>=0) & (m<=100))
-    {
-        if(m>n)
-    {
-        console.log(`Jackpot is Smaller than ${m}`)
+function startGame(level) {
+    var n = 73;
+    var attempts;
+    switch(level) {
+        case 1:
+            attempts = 15;
+            break;
+        case 2:
+            attempts = 10;
+            break;
+        case 3:
+            attempts = 5;
+            break;
+        default:
+            attempts = 0;
     }
-    else if(m<n)
-    {
-        console.log(`Jackpot is Greater than ${m}`)
-    }
-    else if(m=n)
-    {
-        console.log("YOU WON THE JACKPOT!!!")
-        break;
-    }
-    }
-    else
-    {
-        console.log("Invalid Input")
-    }
-}
-}
-else if(a=2)
-{
-    console.log("Medium Level.\nYou Have 10 attempts.")
-    for(i=0;i<=10;i++)
-{
-    m=Number(prompt("Guess the Number(0 to 100) : "))
-    if((m<=100)&(m>=0))
-    {
-        if(m>n)
-    {
-        console.log(`Jackpot is Smaller than ${m}`)
-    }
-    else if(m<n)
-    {
-        console.log(`Jackpot is Greater than ${m}`)
-    }
-    else if(m=n)
-    {
-        console.log("YOU WON THE JACKPOT!!!")
-        break;
-    }
-    }
-    else
-    {
-        console.log("Invalid Input")
-    }
-}
-}
-else if(a=3)
-{
-    console.log("Hard Level.\nYou Have 5 attempts.")
-    for(i=0;i<=5;i++)
-        {
-            m=Number(prompt("Guess the Number(0 to 100) : "))
-            if((m<=100) & m>=0)
-                {
-                    if(m>n)
-                        {
-                        console.log(`Jackpot is Smaller than ${m}`)
-                        }
-            else if(m<n)
-                        {
-                        console.log(`Jackpot is Greater than ${m}`)
-                        }
-            else if(m=n)
-                        {
-                        console.log("YOU WON THE JACKPOT!!!")
-                        break;
-                        }
+    if (attempts > 0) {
+        var output = document.getElementById('output');
+        output.innerHTML = `Level ${level}: You have ${attempts} attempts.<br>`;
+        for (var i = 0; i < attempts; i++) {
+            var guess = Number(prompt("Guess the Number (0 to 100):"));
+            if (!isNaN(guess) && guess >= 0 && guess <= 100) {
+                if (guess > n) {
+                    output.innerHTML += `Jackpot is Smaller than ${guess}<br>`;
+                } else if (guess < n) {
+                    output.innerHTML += `Jackpot is Greater than ${guess}<br>`;
+                } else if (guess === n) {
+                    output.innerHTML += "YOU WON THE JACKPOT!!!<br>";
+                    break;
                 }
-    else
-    {
-        console.log("Invalid Input.")
+            } else {
+                output.innerHTML += "Invalid Input.<br>";
+            }
+        }
+    } else {
+        alert("Invalid level selected.");
     }
 }
-}
-else
-{
-    console.log("Invalid Input.")
-}
-}
+
 
